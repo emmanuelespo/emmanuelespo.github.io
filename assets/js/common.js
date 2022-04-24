fe80265e1cff39e9dd37610430314f5122bc2101
Emmanuel Esposito <emmanuel.espo@gmail.com>
2022-04-24 19:54:09 +0200

    Tentative bib button click-outside event

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,21 @@
+checkPageClickBibtex = false;
+
 $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+        checkPageClickBibtex = true;
     });
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
+
+    // Added by me
+    $(window).click(function() {
+        target = $(document).find(".bibtex.hidden.open");
+        if(checkPageClickBibtex && target && !event.composedPath().includes(target)) {
+            target.toggle("open");
+            checkPageClickBibtex = false;
+        }
+    });
 });
