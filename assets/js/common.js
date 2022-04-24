56af53ca0577048087b4a325b540ca8ca8adb4bd
Emmanuel Esposito <emmanuel.espo@gmail.com>
2022-04-24 20:02:48 +0200

    Remove tentative bib button event

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,3 @@
-checkPageClickBibtex = false;
-
 $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
@@ -9,13 +7,4 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
-
-    // Added by me
-    $(window).click(function() {
-        target = $(document).find(".bibtex.hidden.open");
-        if(checkPageClickBibtex && target && !event.composedPath().includes(target)) {
-            target.toggle("open");
-            checkPageClickBibtex = false;
-        }
-    });
 });
